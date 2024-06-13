862c6ef6c07ad69f69e7813e23cdd1399690754a
castlehunter <vvxiaobai@gmail.com>
2024-06-12 21:39:54 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [myRating, setMyRating] = useState(0);
+  const [friendRating, setFriendRating] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Question value={bill} onChange={(e) => setBill(Number(e.target.value))}>
+        How much was the bill?
+      </Question>
+
+      <Question
+        value={myRating}
+        onChange={(e) => setMyRating(Number(e.target.value))}
+      >
+        How did you like the service?
+      </Question>
+
+      <Question
+        value={friendRating}
+        onChange={(e) => setFriendRating(Number(e.target.value))}
+      >
+        How did your friend like the service?
+      </Question>
+
+      <h2>
+        You pay $____ (${bill} + {myRating} {friendRating})
+      </h2>
+    </>
+  );
+}
+
+function Question({ value, onChange, children }) {
+  return (
+    <div>
+      <form>
+        <span>{children} </span>
+        <input value={value} onChange={onChange} type="number" />
+      </form>
     </div>
   );
 }
